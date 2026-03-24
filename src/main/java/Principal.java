3550f8ef9486edfda2f67352eead9521311306fe
DR DAL <DR DAL@10.146.193.178>
2026-03-24 20:47:58 -0300

    init.inicio do projeto

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -13,11 +13,13 @@ public class Principal {
         }
         String elementoProcurado = JOptionPane.showInputDialog("Digite o nome a ser procurado");
         flag = 0;
-        for (i = 0; i < numElementos; i++) {
-            if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)) {
+        i = 0;
+        while ((i < numElementos) && (flag != 1)) {
+         if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)) {
                 JOptionPane.showMessageDialog(null, "o valor procurado foi encontrado na posição " + i);
                 flag = 1;
             }
+         i++;
         }
         if (flag == 0) {
             JOptionPane.showMessageDialog(null, "o nome não foi encontrado.");
